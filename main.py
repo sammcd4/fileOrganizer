24417e6f1922e5fde4fdab0faa16393029f66a8f
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2019-11-04 23:45:23 -0500

    Added test photo files

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import fileorganizer as fo
 import fileorganizer.comparator as fo_c
 
 mode = 'compare'
-dir1 = '/Volumes/Seagate 2/Photos + Videos/Exported Photo Library/2019/October test'
-dir2 = '/Volumes/Seagate 2/Photos + Videos/Exported Photo Library/2019/10_23 October'
+dir1 = '/Volumes/Seagate 2/Photos + Videos/Exported Photo Library/2019/10 October before'
+dir2 = '/Volumes/Seagate 2/Photos + Videos/Exported Photo Library/2019/10 October'
 
 comp = fo_c.Comparator()
 comp.compare_folders(dir1, dir2)
