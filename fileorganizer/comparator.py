58a19d1dd3b23783e036ffe8e0f9206b96c0a553
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2019-11-04 21:51:26 -0500

    shallow=False implementation of dircmp class

--- fileorganizer/comparator.py
+++ fileorganizer/comparator.py
@@ -6,11 +6,25 @@ from pathlib import Path
 from collections import namedtuple
 
 
+class Dircmp(filecmp.dircmp):
+    """
+    Compare the content of dir1 and dir2. In contrast with filecmp.dircmp, this
+    subclass compares the content of files with the same path.
+    """
+    def phase3(self):
+        """
+        Find out differences between common files.
+        Ensure we are using content comparison with shallow=False.
+        """
+        fcomp = filecmp.cmpfiles(self.left, self.right, self.common_files, shallow=False)
+        self.same_files, self.diff_files, self.funny_files = fcomp
+
+
 class Comparator:
     move_duplicates = True
 
     def __init__(self):
-        self.move_duplicates = True
+        self.move_duplicates = False
 
     def print_diff_files(self, dcmp):
         for name in dcmp.diff_files:
@@ -32,7 +46,25 @@ class Comparator:
             print('dir2 is empty')
             return
 
-        dcmp = filecmp.dircmp(dir1, dir2)
+        if False:
+            # Determine the items that exist in both directories
+            d1_contents = set(os.listdir(dir1))
+            d2_contents = set(os.listdir(dir2))
+            common = list(d1_contents & d2_contents)
+            common_files = [f
+                            for f in common
+                            if os.path.isfile(os.path.join(dir1, f))
+                            ]
+            print('Common files:{}'.format(common_files))
+
+            # Compare the directories
+            match, mismatch, errors = filecmp.cmpfiles(dir1, dir2, common_files, shallow=False)
+
+            print('Match:{}'.format(match))
+            print('Mismatch:{}'.format(mismatch))
+            print('Errors:{}'.format(errors))
+        
+        dcmp = Dircmp(dir1, dir2)
         self.print_diff_files(dcmp)
 
         # get current time for duplicates folder name
@@ -52,7 +84,7 @@ class Comparator:
         compareInfo = CompareInfo(dir1, dir2, timestamp_str, comparison_dir)
 
         if self.move_duplicates:
-            self.move_duplicate_files(dcmp, compareInfo)
+            self.move_duplicate_files2(compareInfo)
 
     def move_duplicate_files(self, dcmp, compare_info):
 
@@ -65,4 +97,16 @@ class Comparator:
 
         for sub_dcmp in dcmp.subdirs.values():
             self.move_duplicate_files(sub_dcmp, compare_info)
+
+    def move_duplicate_files2(self, compare_info):
+
+        for name in compare_info.match:
+            print("same_file {} found in {} and {}".format(name, compare_info.dir1, compare_info.dir2))
+            md = compare_info.move_dir
+            if not os.path.isdir(md):
+                os.mkdir(md)
+            shutil.move(Path(compare_info.dir1, name), Path(md, name))
+
+        #for sub_dcmp in dcmp.subdirs.values():
+        #    self.move_duplicate_files(sub_dcmp, compare_info)
 
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import fileorganizer as fo
 import fileorganizer.comparator as fo_c
 
 mode = 'compare'
-dir1 = '/Volumes/Seagate 4/Seagate 2 Backup/_gsdata_/_saved_/Photos + Videos/Exported Photo Library/2019/09_19 September'
-dir2 = '/Volumes/Seagate 2/Photos + Videos/Exported Photo Library/2019/09 September'
+dir1 = '/Volumes/Seagate 2/Photos + Videos/Exported Photo Library/2019/October test'
+dir2 = '/Volumes/Seagate 2/Photos + Videos/Exported Photo Library/2019/10_23 October'
 
 comp = fo_c.Comparator()
 comp.compare_folders(dir1, dir2)
