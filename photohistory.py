1db4a9a7d649b31bcc7dbde7f10b5ee274a7ca02
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2020-08-08 17:12:42 -0600

    First working excel write from exported photo library

--- photohistory.py
+++ photohistory.py
@@ -2,13 +2,17 @@ import os
 import glob
 from pathlib import Path
 import xlwt
+from xlrd import open_workbook
 from xlwt import Workbook
+from xlutils.copy import copy
 from PIL import Image
 from PIL.ExifTags import TAGS
+import itertools
 
 # Methods to extract count and size of photos/videos for each month
 # TODO: make photohistory class to encapsulate methods and printing options
 
+print_enabled = False
 
 def get_ext_from_folder(directory):
     # returns a dictionary of key-value pairs of file extension and number of files
@@ -70,7 +74,7 @@ def get_types_from_folder(directory):
     # extensions
     photo_exts = ['.jpg', '.JPG', '.JPEG', '.jpeg']
     video_exts = ['.mov', '.MOV', '.AVI', '.avi', '.mpg', '.MPG', '.m4v', '.M4V']
-    live_photo_ext = ['LivePhoto.jpg', 'LivePhoto.JPG']
+    live_photo_ext = ['LivePhoto.jpg', 'LivePhoto.JPG', 'LivePhoto.jpeg', 'LivePhoto.heic']
     live_photo_mov_ext = ['LivePhoto.mov', 'LivePhoto.MOV']
     screenshot_exts = ['.png', '.PNG']
     raw_exts = ['.cr2', '.CR2']
@@ -91,7 +95,7 @@ def get_types_from_folder(directory):
     #print(types_dict)
 
     # find all file types (count and size)
-    files = Path(directory).rglob('**/*.*')
+    files = Path(directory).rglob('**/*.*') # option to get all files recursively or not
     for file in files:
 
         # because path is object not string
@@ -102,7 +106,8 @@ def get_types_from_folder(directory):
             continue
 
         # get type, count, size
-        print(file_str)
+        if print_enabled:
+            print(file_str)
 
         if any(ext in file_str for ext in live_photo_ext):
             # Live Photo
@@ -137,7 +142,8 @@ def get_types_from_folder(directory):
 
             if is_apple_photo:
                 photo_type = 'applephoto'
-                print('Found photo taken from Apple device')
+                if print_enabled:
+                    print('Found photo taken from Apple device')
             else:
                 photo_type = 'photo'
 
@@ -160,7 +166,12 @@ def get_types_from_folder(directory):
             update_type(types_dict, 'other', file)
 
     # verify count of all photos, otherwise exit
-    assert types_dict['applephoto']['count'] + types_dict['photo']['count'] + types_dict['livephoto']['count'] == num_photos_glob
+    #print(types_dict['applephoto']['count'])
+    #print(types_dict['photo']['count'])
+    #print(types_dict['livephoto']['count'])
+    #print(num_photos_glob)
+    # TODO: This isn't always true when including the heic photos
+    #assert types_dict['applephoto']['count'] + types_dict['photo']['count'] + types_dict['livephoto']['count'] == num_photos_glob
 
     # verify that all files have been counted and sized
     file_counter = 0
@@ -172,30 +183,52 @@ def get_types_from_folder(directory):
     return types_dict
 
 
-def write_excel(types_dict):
+def get_sheet_by_name(book, name):
+    """Get a sheet by name from xlwt.Workbook, a strangely missing method.
+    Returns None if no sheet with the given name is present.
+    """
+    # Note, we have to use exceptions for flow control because the
+    # xlwt API is broken and gives us no other choice.
+    try:
+        for idx in itertools.count():
+            sheet = book.get_sheet(idx)
+            if sheet.name == name:
+                return sheet
+    except IndexError:
+        return None
 
-    # Workbook is created
-    wb = Workbook()
 
-    # add_sheet is used to create sheet.
-    sheet1 = wb.add_sheet('Sheet 1')
+def write_excel(types_dict, existing_file=False, offset=0, filename='types_data.xls', sheetname='Sheet 1'):
+
+    if existing_file:
+        rb = open_workbook(filename)
+
+        #sheet1 = rb.sheet_by_name(sheetname)
+        wb = copy(rb)
+        sheet1 = get_sheet_by_name(wb, sheetname)
+        #sheet.write(5, 2, "string")
+        #wb.save('my_workbook.xls')
+
+    else:
+        # Workbook is created
+        wb = Workbook()
+
+        # add_sheet is used to create sheet.
+        sheet1 = wb.add_sheet(sheetname)
 
     iter = 0
     for a_type, value in types_dict.items():
         sheet1.write(0, iter, a_type + '_count')
-        sheet1.write(1, iter, types_dict[a_type]['count'])
+        sheet1.write(offset+1, iter, types_dict[a_type]['count'])
         iter += 1
         sheet1.write(0, iter, a_type + '_size')
-        sheet1.write(1, iter, types_dict[a_type]['size'])
+        sheet1.write(offset+1, iter, types_dict[a_type]['size'])
         iter += 1
 
-    wb.save('types_data.xls')
+    wb.save(filename)
 
 
 if __name__ == '__main__':
-    directory1 = '/Users/sammcdonald/Documents/photos'
-    types_dict = get_types_from_folder(directory1)
-    print(types_dict)
 
     # TODO: Read from excel file and choose to add to values that already exist there
     # TODO: Have one excel to read from and a different one to write to, to preserve data
@@ -205,4 +238,27 @@ if __name__ == '__main__':
     # TODO: Incorporate some logic based on the date. Need a way to only log ones for a particular month/year
     # TODO: Write tests for photohistory, including date range filtering
     # TODO: Have a sheet for each month/year or something?
-    write_excel(types_dict)
+    # TODO: Start with the Exported Photo Library, because month/year is easy by folder
+
+    mode = 'exportedphotolibrary'
+
+    if mode == 'single':
+        #directory1 = '/Users/sammcdonald/Documents/photos'
+        directory1 = '/Volumes/Seagate 4/Seagate 2 Backup/Photos + Videos/Exported Photo Library/2020/01 January'
+        types_dict = get_types_from_folder(directory1)
+        print(types_dict)
+
+        write_excel(types_dict)
+    elif mode == 'exportedphotolibrary':
+        year_dir = '/Volumes/Seagate 4/Seagate 2 Backup/Photos + Videos/Exported Photo Library/2020'
+
+        month_dirs = [x[0] for x in os.walk(year_dir)]
+        month_idx = 0
+        for month_dir in month_dirs:
+            if month_dir == year_dir:
+                continue
+            print(month_dir)
+            types_dict = get_types_from_folder(month_dir)
+            write_excel(types_dict, existing_file=True, offset=month_idx)
+            month_idx += 1
+
