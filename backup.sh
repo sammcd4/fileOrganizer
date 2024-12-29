cc3ecbbeb8380bd2fcd98b1a2a8495627c58de55
McDonald Family <mcdonaldfamily@McDonalds-iMac.local>
2024-12-28 20:39:44 -0700

    edits to backup and main comparison scenarios

--- backup.sh
+++ backup.sh
@@ -4,26 +4,27 @@
 
 dry_run=""
 verbose=0
-deleted_backup_dir="../recently_deleted_files/"
+deleted_backup_dirname="recently_deleted_files"
 
-Help()
+usage()
 {
    # Display Help
    echo "Backup SRC (first argument) to DEST (second argument) and keep any deleted files from SRC"
    echo
-   echo "Syntax: backup [-h|d|v] <SRC> <DEST>"
+   echo "Syntax: backup [-h|d|v] [-b <BACKUP_DIR>] <SRC> <DEST>"
    echo "options:"
    echo "h     Print this Help."
    echo "n     Dry run only."
    echo "v     Verbose mode."
+   echo "b     Specify the directory to backup recently deleted files. Default is '../recently_deleted_files/'."
    echo
 }
 
 # Get the options
-while getopts ":hnv" option; do
+while getopts ":hnvb:" option; do
    case $option in
-      h) # display Help
-         Help
+      h) # display usage
+         usage
          exit;;
       v) # enable verbose mode
          verbose=1
@@ -31,12 +32,26 @@ while getopts ":hnv" option; do
       n) # use rsync dry run
          dry_run="-n"
          ;;
+      b) # specify the backup directory
+         deleted_backup_dirname="$OPTARG"
+         ;;
+      \?) # invalid option
+         echo "Error: Invalid option -$OPTARG" >&2
+         usage
+         exit 1
+         ;;
+      :) # option requires an argument
+         echo "Error: Option -$OPTARG requires an argument." >&2
+         usage
+         exit 1
+         ;;
    esac
 done
 shift $((OPTIND - 1))
 
 src=$1
 dest=$2
+deleted_backup_dir="../$deleted_backup_dirname/"
 
 if [ "$verbose" -eq 1 ]; then
     # Explanation of backup
@@ -48,9 +63,10 @@ fi
 
 # Check is correct arguments are supplied
 if [ $# -eq 0 ]; then
-    echo "No arguments supplied"
+    usage
 elif [ $# -eq 1 ]; then
     echo "Only 1 argument given. SRC=$src"
+    usage
 elif [ $# -eq 2 ]; then
     # Run backup command
     backup_cmd="rsync -a -v $dry_run --delete --backup --backup-dir=$deleted_backup_dir \"$src/\" \"$dest/\""
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import datetime
 import time
 
-mode = 'compare'
+mode = 'multcompare_both'
 
 # TODO: create one to many comparison scheme instead of just one to one or many in different folders
 
@@ -25,11 +25,41 @@ if mode == 'exported':
 elif mode == 'compare':
 
     # define directories to compare
-    duplicate_dir = '/Users/sammcdonald/Desktop/Seagate 5/Seagate 2 Backup/1_Sam/Piano Sheet Music'
-    dir_untouched_original = '/Volumes/Seagate 5/Seagate 2 Backup/1_Sam/Piano Sheet Music'
+    duplicate_dir = '/Volumes/My Book 6/_saved_/Photos + Videos/100__TSB _to organize'
+    dir_untouched_original = '/Volumes/Seagate 5/Seagate 2 Backup/Photos + Videos/2019'
 
     # compare
-    foc.compare(duplicate_dir, dir_untouched_original, ignore_extensions=True)
+    foc.compare(duplicate_dir, dir_untouched_original, ignore_extensions=False)
+elif mode == 'multcompare_orig_organized':
+    # directory with raw files to compare (to delete)
+    single_duplicate_dir = '/Volumes/My Book 6/_saved_/Photos + Videos/2012/2012_First Married Year Adventures/Sam\'s Birthday'
+
+    # directory containing organized subdirectories containing files (originals)
+    untouched_original_dir = '/Volumes/Seagate 5/Seagate 2 Backup/Photos + Videos/2012'
+
+    # iterate over all subdirectories in original dir and search for files to delete from single duplicate dir
+    # feature{print_compare_summary_for_multiple_subdir_comparisons} provide a summary when foc.compare called multiple times. Use output to summarize total, or absorb this functionality into foc class
+    for subdir, dirs, files in os.walk(untouched_original_dir):
+        for untouched_original_subdir in dirs:
+            # print(f'comparing orig subdir: {os.path.join(subdir, untouched_original_subdir)} with duplicate_dir: {single_duplicate_dir}')
+            foc.compare(single_duplicate_dir, os.path.join(subdir, untouched_original_subdir), ignore_extensions=False)
+
+elif mode == 'multcompare_both':
+    # directory with subdirectories to compare (to delete)
+    duplicate_dir = '/Volumes/My Book 6/_saved_/Photos + Videos/2015'
+
+    # directory containing organized subdirectories containing files (originals)
+    untouched_original_dir = '/Volumes/Seagate 5/Seagate 2 Backup/Photos + Videos/2015'
+
+    for d_subdir, d_dirs, _ in os.walk(duplicate_dir):
+        for duplicate_subdir in d_dirs:
+
+            # iterate over all subdirectories in original dir and search for files to delete from single duplicate dir
+            # feature{print_compare_summary_for_multiple_subdir_comparisons} provide a summary when foc.compare called multiple times. Use output to summarize total, or absorb this functionality into foc class
+            for subdir, dirs, files in os.walk(untouched_original_dir):
+                for untouched_original_subdir in dirs:
+                    # print(f'comparing orig subdir: {os.path.join(subdir, untouched_original_subdir)} with duplicate_dir: {os.path.join(d_subdir, duplicate_subdir)}')
+                    foc.compare(os.path.join(d_subdir, duplicate_subdir), os.path.join(subdir, untouched_original_subdir), ignore_extensions=False)
 
 elif mode == 'update_dates':
     directory = '/Volumes/Seagate 4/Seagate 2 Backup/Photos + Videos/2016 to sort'
@@ -89,9 +119,9 @@ elif mode == 'compare_mult_months':
 # TODO: in multcompare mode, literally want to call compare multiple times so that comparision folder is in the correct spot?
 elif mode == 'multcompare':
 
-    duplicate_dir = '/Volumes/Seagate 5/Seagate 2 Backup/_gsdata_/_saved_/Photos + Videos/Exported Photo Library/2017'
-    dir_untouched_orig_base = '/Volumes/Seagate 4/Seagate 2 Backup/Photos + Videos/Exported Photo Library/2017/'
-    dir_untouched_original = dir_untouched_orig_base + '03 March'
+    duplicate_dir = '/Volumes/My Book 6/_saved_/Photos + Videos/Exported Photo Library/2016'
+    dir_untouched_orig_base = '/Volumes/My Book 6/Backup/Photos + Videos/Exported Photo Library/2016/'
+    
 
     # TODO: Got to add tests for multcompare if I'm using it so heavily here!
     if True:
@@ -106,7 +136,11 @@ elif mode == 'multcompare':
                     path_dir = Path(duplicate_dir, f)
                     if os.path.isdir(path_dir) and orig_dir.split('/')[-1] in str(path_dir):
                         foc.compare(path_dir, orig_dir, True, ignore_extensions=True)
+                        # TODO fix which paths are compared here
+                        # print(orig_dir.split('/')[-1])
+                        # print(f'dup: {path_dir}\ncompared with\norig_dir: {orig_dir}')
     else:
+        dir_untouched_original = dir_untouched_orig_base + '03 March'
         # only one original directory
         foc.multcompare(duplicate_dir, dir_untouched_original)
 
@@ -116,15 +150,26 @@ elif mode == 'rename':
 elif mode == 'compare_exported':
 
     # parameters
-    volume_dir = '/Volumes/Seagate 4/Seagate 2 Backup/Photos + Videos/Exported Photo Library/2020/'
-    month_dir = '08 August'
-    old_marker = ' old 4'
+    drive = '/Volumes/My Book 6/Backup/'
+    volume_dir = 'Photos + Videos/Exported Photo Library/2016/'
+    month_dir = '03 March'
+    old_marker = ''
+
+    dup_base_dir = '/Volumes/My Book 6/_saved_/Photos + Videos/Exported Photo Library/2016/'
 
     # directories to compare
-    duplicate_dir = volume_dir + month_dir + old_marker
-    dir_untouched_original = volume_dir + month_dir
+    # duplicate_dir = drive + volume_dir + month_dir + old_marker
+    duplicate_dir = dup_base_dir + month_dir + old_marker
+    # duplicate_dir = '/Volumes/My Book 6/_saved_/Photos + Videos/Exported Photo Library/2020 imported 2/06 June imported'
+    dir_untouched_original = drive + volume_dir + month_dir
 
     # compare
-    foc.compare(duplicate_dir, dir_untouched_original)
+    foc.compare(duplicate_dir, dir_untouched_original, ignore_extensions=False)
+
+elif mode == 'mult_compare_exported':
+
+    duplicate_dir = '/Volumes/My Book 6/_saved_/Photos + Videos/Exported Photo Library/2020/'
+    dir_untouched_orig_base = '/Volumes/My Book 6/Backup/Photos + Videos/Exported Photo Library/2020/'
+
 
 # TODO: improve src/dup print outs when same_file found
